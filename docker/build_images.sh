d1c0763bd0fd021ee525ef2f0dcbca1b55fc55b3
Cheong-mok Bae <cjdahrl@gmail.com>
2024-12-05 14:25:32 +0900

    wip

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -5,28 +5,9 @@ set -e
 
 # Build images
 docker build -f dockerfiles/wine.dockerfile -t starcraft:wine .
-if [ $? -ne 0 ]; then
-    echo "Failed to build starcraft:wine image"
-    exit 1
-fi
-
 docker build -f dockerfiles/bwapi.dockerfile -t starcraft:bwapi .
-if [ $? -ne 0 ]; then
-    echo "Failed to build starcraft:bwapi image"
-    exit 1
-fi
-
 docker build -f dockerfiles/play.dockerfile -t starcraft:play .
-if [ $? -ne 0 ]; then
-    echo "Failed to build starcraft:play image"
-    exit 1
-fi
-
 docker build -f dockerfiles/java.dockerfile -t starcraft:java .
-if [ $? -ne 0 ]; then
-    echo "Failed to build starcraft:java image. You can verify that you will build with the proper version of Java in the file dockerfiles/play.dockerfile"
-    exit 1
-fi
 
 # Change directory
 pushd ../observer/local_docker
@@ -38,10 +19,6 @@ fi
 
 # Build the final game image
 docker build -f game.dockerfile -t "starcraft:game" .
-if [ $? -ne 0 ]; then
-    echo "Failed to build starcraft:game image"
-    exit 1
-fi
 
 # Return to the original directory
 popd
